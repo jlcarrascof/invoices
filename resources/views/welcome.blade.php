c99822dafb9cf1a75e219637067ad843df955961
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-14 13:40:32 -0400

    Update page title to 'Invoicing and Inventory System' in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with Tailwind</title>
+    <title>Invoicing and Inventory System</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
