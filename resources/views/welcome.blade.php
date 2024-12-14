3c03596e462e2b3770ac12f5452d205ea297233d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-14 13:39:44 -0400

    Remove welcome message and example description from the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,6 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
-    <div class="text-center">
-        <h1 class="text-4xl font-bold text-blue-500">
-            !Welcome to Laravel 11!
-        </h1>
-        <p class="text-gray-600 mt-4">
-            This is an example using Tailwind CSS
-        </p>
-    </div>
+
 </body>
 </html>
