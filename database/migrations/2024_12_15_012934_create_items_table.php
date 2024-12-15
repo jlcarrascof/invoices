ea846fc886a66fa2a9f0f8ad5c34d0065eb32177
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-14 21:32:13 -0400

    Add category_id and additional fields to items table migration

--- database/migrations/2024_12_15_012934_create_items_table.php
+++ database/migrations/2024_12_15_012934_create_items_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('code', 50)->nullable();
+            $table->string('name', 100);
+            $table->integer('stock');
+            $table->string('description', 512)->nullable();
+            $table->string('image', 256)->nullable();
+            $table->boolean('status')->default(1);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
