3ae0fac80f25c842519aaeaba68efd6a81e6c3e6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-22 13:44:50 -0400

    Define table name and fillable properties in Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $table = 'items';
+    protected $fillable = ['category_id', 'code', 'name', 'stock', 'description', 'image', 'status'];
+
+
 }
