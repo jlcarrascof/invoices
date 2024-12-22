7fdd473b1cd1af49e0b2d95942b066908261814e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-22 13:53:34 -0400

    Define table name and fillable properties in Supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Supplier extends Model
 {
     use HasFactory;
+
+    protected $table = 'suppliers';
+    protected $fillable = ['name', 'document_type', 'document_number', 'address', 'phone', 'email', 'status'];
 }
