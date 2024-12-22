6db25d9887ec9d49ec9db71fad69c4b7bdf77c01
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-22 13:50:29 -0400

    Define table name and fillable properties in Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $table = 'customers';
+    protected $fillable = ['name', 'document_type', 'document_number', 'address', 'phone', 'email', 'status'];
 }
