68ce73e572ddb8420904d21405a417eab5933c3e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-29 17:52:52 -0400

    Add session flash message display to SimpleForm Livewire component

--- resources/views/livewire/simple-form.blade.php
+++ resources/views/livewire/simple-form.blade.php
@@ -1,3 +1,9 @@
 <div class="p-4 bg-white rounded shadow">
 
+    @if (session()->has('message'))
+        <div class="bg-green-500 text-white p-2 rounded mb-4">
+            {{ session('message') }}
+        </div>
+    @endif
+
 </div>
