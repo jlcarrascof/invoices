7336039ebed550b94c9f8957f2bf9d0d4440a978
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-29 17:45:51 -0400

    Add SimpleForm Livewire component and view

---
+++ app/Livewire/SimpleForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SimpleForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.simple-form');
+    }
+}
