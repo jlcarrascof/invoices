5fcafac9782ca6943cac9b621acbb8a78444d3d2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-29 17:56:32 -0400

    Add submit button to SimpleForm Livewire component

--- resources/views/livewire/simple-form.blade.php
+++ resources/views/livewire/simple-form.blade.php
@@ -7,11 +7,14 @@
     @endif
 
     <form wire:submit.prevent="save">
-        <label for="name" class="block text-sm font-medium text-gray-700">Nombre</label>
+        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
         <input type="text" id="name" wire:model="name" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
         @error('name')
             <span class="text-red-500 text-sm">{{ $message }}</span>
         @enderror
 
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded mt-4">
+            Save
+        </button>
     </form>
 </div>
