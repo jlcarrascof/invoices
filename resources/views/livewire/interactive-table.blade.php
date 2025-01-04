ff26e2d1aa23978dba3b8120140df3770b61544d
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-04 12:32:06 -0400

    Update column headers in InteractiveTable Livewire component to English

--- resources/views/livewire/interactive-table.blade.php
+++ resources/views/livewire/interactive-table.blade.php
@@ -3,8 +3,8 @@
         <thead>
             <tr class="bg-gray-200">
                 <th class="px-4 py-2">ID</th>
-                <th class="px-4 py-2">Nombre</th>
-                <th class="px-4 py-2">Estado</th>
+                <th class="px-4 py-2">Name</th>
+                <th class="px-4 py-2">Status</th>
             </tr>
         </thead>
     </table>
