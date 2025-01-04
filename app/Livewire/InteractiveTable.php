c156246235212dbe0353a3313c3e6cea5d8c2784
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-04 12:25:01 -0400

    Add initial data to InteractiveTable Livewire component

--- app/Livewire/InteractiveTable.php
+++ app/Livewire/InteractiveTable.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class InteractiveTable extends Component
 {
+    public $data = [
+        ['id' => 1, 'name' => 'Category A', 'status' => 'Active'],
+        ['id' => 2, 'name' => 'Category B', 'status' => 'Inactive'],
+    ];
+
     public function render()
     {
         return view('livewire.interactive-table');
