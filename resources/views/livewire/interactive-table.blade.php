3ecdd54342b60ba21a2c8846c483f58223de2557
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-04 12:31:41 -0400

    Add table header to InteractiveTable Livewire component

--- resources/views/livewire/interactive-table.blade.php
+++ resources/views/livewire/interactive-table.blade.php
@@ -1,5 +1,11 @@
 <div>
     <table class="table-auto w-full bg-white rounded shadow">
-
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="px-4 py-2">ID</th>
+                <th class="px-4 py-2">Nombre</th>
+                <th class="px-4 py-2">Estado</th>
+            </tr>
+        </thead>
     </table>
 </div>
