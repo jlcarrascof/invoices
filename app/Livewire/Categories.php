8e4f6282bd45fb67fef6cadd55115e5ec665dccd
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-05 17:49:26 -0400

    Load categories data in the mount method of Categories Livewire component

--- app/Livewire/Categories.php
+++ app/Livewire/Categories.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Categories extends Component
 {
@@ -14,6 +15,11 @@ class Categories extends Component
         'name' => 'required|max:50',
         'description' => 'nullable|max:255',
     ];
+
+    public function mount()
+    {
+        $this->categories = Category::all();
+    }
 
     public function render()
     {
