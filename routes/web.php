71de572527b1921d6f28503838c12b2744b9ab7b
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-05 13:41:22 -0400

    Add route for Categories Livewire component

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Categories;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories', Categories::class)->name('categories');
