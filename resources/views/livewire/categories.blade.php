15997658b82065593ee0ce69cad0636834bb7287
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-05 17:52:01 -0400

    Add success message display to categories component

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -1,3 +1,8 @@
 <div class="p-4 bg-gray-100">
-    {{-- Success is as dangerous as failure. --}}
+    <!-- Success Message -->
+    @if (session()->has('message'))
+        <div class="bg-green-500 text-white p-2 rounded mb-4">
+            {{ session('message') }}
+        </div>
+    @endif
 </div>
