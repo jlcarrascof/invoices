f010a5e678669bf7007c04779e8cc610e3b0d3da
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-05 17:57:15 -0400

    Add table header for categories in Livewire component

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -33,6 +33,12 @@
 
     <!-- Categories Table -->
     <table class="table-auto w-full bg-white rounded shadow">
-
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="px-4 py-2">ID</th>
+                <th class="px-4 py-2">Nombre</th>
+                <th class="px-4 py-2">Descripción</th>
+            </tr>
+        </thead>
     </table>
 </div>
