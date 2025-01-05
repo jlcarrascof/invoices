5cd7bce8ae9fc59d7f6027f1d2febe0ddc96a638
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-05 17:55:25 -0400

    Add submit button to categories form

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -25,5 +25,9 @@
                 <span class="text-red-500 text-sm">{{ $message }}</span>
             @enderror
         </div>
+
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">
+            Guardar
+        </button>
     </form>
 </div>
