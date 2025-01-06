1442f399b9995df344d30799ba7091b5cfd45199
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-06 17:28:59 -0400

    Add header and description to Categories Livewire view for better context

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -1,5 +1,8 @@
 @extends('components.layouts.app')
 
 @section('content')
-<!-- Aquí va tu contenido -->
+<div>
+    <h1 class="text-2xl font-bold">Gestión de Categorías</h1>
+    <p>Este es el módulo de categorías.</p>
+</div>
 @endsection
