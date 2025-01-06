8b1ac5b4d58923337e2a09b9f8a3c4678e509433
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-06 10:46:25 -0400

    Remove categories route and associated Livewire component

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Livewire\Categories;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/categories', Categories::class)->name('categories');
