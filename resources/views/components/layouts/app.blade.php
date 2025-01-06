6c92dbfe2846ff7e22735868cfcbde45a919ba33
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-06 10:38:09 -0400

    Rename app layout file and update title to be dynamic

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoicing and Inventory System</title>
+    <title>{{ $title ?? 'Invoicing System' }}</title>
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
