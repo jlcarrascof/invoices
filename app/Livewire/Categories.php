34f4d312a835cf476371264c32f8b42facc6ac7b
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-06 13:21:01 -0400

    Remove manual layout extension from Categories Livewire component

--- app/Livewire/Categories.php
+++ app/Livewire/Categories.php
@@ -8,8 +8,6 @@ class Categories extends Component
 {
     public function render()
     {
-        return view('livewire.categories')
-            ->extends('components.layouts.app') // Extiende el layout manualmente
-            ->section('content'); // Define la sección de contenido
+        return view('livewire.categories');
     }
 }
