1c50767cd08ba6fc937f1c6923871117c420f101
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-07 10:56:10 -0400

    Refactor welcome view to use content section for dynamic rendering

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,10 +31,7 @@
 
         <!-- Main content -->
         <main class="flex-1 p-6">
-            <h2 class="text-2xl font-semibold mb-4">
-                Blank Page
-            </h2>
-            <p class="text-gray-600">This is the zone to create our web app</p>
+            @yield('content')
         </main>
     </div>
 
