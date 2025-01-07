81e753fffb9b545afbc5fb5d33432a96ba29f4af
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-07 10:58:44 -0400

    Add error message display to category creation view

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 
 <div class="max-w-2xl mx-auto bg-white p-6 rounded shadow">
+    <h1 class="text-2xl font-bold mb-4">Crear Categoría</h1>
+
+    <!-- Mostrar mensajes de error -->
+    @if ($errors->any())
+        <div class="bg-red-100 text-red-700 p-3 rounded mb-4">
+            <ul class="list-disc pl-5">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
 
 </div>
 
