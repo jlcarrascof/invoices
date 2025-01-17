de3a3105e914ee9bdc24c72be400eb5feb97e2c0
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-17 11:31:47 -0400

    Add category rows to index view with conditional status display

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,6 +14,22 @@
                 <th class="px-4 py-2">Status</th>
             </tr>
         </thead>
+        <tbody>
+            @forelse ($categories as $category)
+                <tr>
+                    <td class="border px-4 py-2">{{ $category->id }}</td>
+                    <td class="border px-4 py-2">{{ $category->name }}</td>
+                    <td class="border px-4 py-2">{{ $category->description }}</td>
+                    <td class="border px-4 py-2">
+                        {{ $category->condition ? 'Active' : 'Inactive' }}
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4" class="text-center py-4">Categories not found.</td>
+                </tr>
+            @endforelse
+        </tbody>
     </table>
 
 </div>
