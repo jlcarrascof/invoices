2d4e91a4669930f7cb70e26a66d907fcbee442ac
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-17 11:28:37 -0400

    Add table structure for categories in index view

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,7 +4,10 @@
 <div class="max-w-6xl mx-auto bg-white p-6 rounded shadow">
     <h1 class="text-2xl font-bold mb-4">Categories List</h1>
 
+    <!-- Categories Table -->
+    <table class="table-auto w-full bg-gray-100 rounded">
 
+    </table>
 
 </div>
 @endsection
