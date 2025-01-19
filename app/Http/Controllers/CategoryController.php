f1fa060091a202e6042b851752cc5d069ebc6fd6
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-19 12:53:32 -0400

    Implement edit view return in CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -52,7 +52,7 @@ class CategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view('categories.edit', compact('category'));
     }
 
     /**
