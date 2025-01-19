204caabbbf303a3deddf1d24e723ec59e8b5118a
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-19 13:33:37 -0400

    Add description field to edit category view

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -22,8 +22,14 @@
 
         <!-- Name -->
         <div>
-            <label for="name" class="block text-sm font-medium text-gray-700">Nombre</label>
+            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
             <input type="text" name="name" id="name" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" value="{{ old('name', $category->name) }}">
+        </div>
+
+        <!-- Description -->
+        <div>
+            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+            <textarea name="description" id="description" rows="3" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">{{ old('description', $category->description) }}</textarea>
         </div>
 
     </form>
