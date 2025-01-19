06ff6b14ba4a9aac5a61731984c043e3f0494099
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-19 12:52:46 -0400

    Add update route for categories

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Route::resource('categories', CategoryController::class);
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
