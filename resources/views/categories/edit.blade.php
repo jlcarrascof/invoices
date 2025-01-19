78c7744dd70f1447aa35a86b96a13f8c0e6b816f
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-19 13:01:17 -0400

    Add validation error message display in edit category view

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -5,6 +5,16 @@
 <div class="max-w-2xl mx-auto bg-white p-6 rounded shadow">
     <h1 class="text-2xl font-bold mb-4">Edit Category</h1>
 
+    <!-- Show validation error messages -->
+    @if ($errors->any())
+        <div class="bg-red-100 text-red-700 p-3 rounded mb-4">
+            <ul class="list-disc pl-5">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
 </div>
 
