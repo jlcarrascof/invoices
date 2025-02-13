1c70aac6c237d19189fc9f736b2477dedc8bf11c
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-13 18:50:08 -0400

    Add success and error message display to supplier creation view

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -5,6 +5,24 @@
 <div class="max-w-2xl mx-auto bg-white p-6 rounded shadow">
     <h1 class="text-2xl font-bold mb-4">Suppliers</h1>
 
+    <!-- Show success messages -->
+    @if (session('success'))
+        <div class="bg-green-100 text-green-700 p-3 rounded mb-4">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <!-- Show validations errors -->
+    @if ($errors->any())
+        <div class="bg-red-100 text-red-700 p-3 rounded mb-4">
+            <ul class="list-disc pl-5">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
 
 </div>
 
