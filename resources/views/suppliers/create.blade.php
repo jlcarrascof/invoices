36dcb2ba6e75b9859ea5a82fb2c99f0448cf178a
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-13 18:47:55 -0400

    Create view for supplier creation form

---
+++ resources/views/suppliers/create.blade.php
@@ -0,0 +1,13 @@
+@extends('welcome')
+
+@section('content')
+
+<div class="max-w-2xl mx-auto bg-white p-6 rounded shadow">
+    <h1 class="text-2xl font-bold mb-4">Suppliers</h1>
+
+
+</div>
+
+
+
+@endsection
