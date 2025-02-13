6a1a95bbcac2d3cd78d9d947ce0999a7347a7803
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-13 19:01:13 -0400

    Add phone and email input fields to supplier creation form

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -57,6 +57,18 @@
             <input type="text" name="address" id="address" class="w-full px-4 py-2 border rounded" value="{{ old('address') }}">
         </div>
 
+        <!-- Phone and Email -->
+        <div class="grid grid-cols-2 gap-4">
+            <div>
+                <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
+                <input type="text" name="phone" id="phone" class="w-full px-4 py-2 border rounded" value="{{ old('phone') }}">
+            </div>
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input type="email" name="email" id="email" class="w-full px-4 py-2 border rounded" value="{{ old('email') }}">
+            </div>
+        </div>
+
     </form>
 
 </div>
