7b26e3e0f3bcf9139a9db1cfc2a7cb6ed43f1dc3
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-02-24 19:28:02 -0400

    Add Alpine.js script to welcome view and create stack dump file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <title>Invoicing and Inventory System</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="bg-gray-100 font-sans">
     <!-- Header -->
