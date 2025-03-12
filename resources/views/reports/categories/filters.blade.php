6aebf4cac3bbb9c7b020f23368fafe08d6f3ff8b
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-12 10:41:01 -0400

    Enhance categories filters view with header and styling

--- resources/views/reports/categories/filters.blade.php
+++ resources/views/reports/categories/filters.blade.php
@@ -2,8 +2,7 @@
 
 
 @section('content')
-
-
-
-
+<div class="max-w-3xl mx-auto bg-white p-6 rounded shadow">
+    <h1 class="text-2xl font-bold mb-4">Generar Reporte de Categorías</h1>
+</div>
 @endsection
